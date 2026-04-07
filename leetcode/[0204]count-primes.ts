61cb53f412fbc68a9d902540b46969356b0fc2cf
zzinnykko <zzinnykko@naver.com>
2026-04-07 20:51:14 +0900

    Create [0204]count-primes.ts

---
+++ leetcode/[0204]count-primes.ts
@@ -0,0 +1,58 @@
+// https://leetcode.com/problems/count-primes/
+function countPrimes(n: number): number {
+    return solution2(n);
+}
+
+
+
+// sieve of eratosthenes: 208 ms
+function solution1(n: number): number {
+    if (n < 2) return 0;
+
+    const primes = Array(n).fill(true);
+    primes[0] = false;
+    primes[1] = false;
+
+    for (let i = 2; i < n ** 0.5; i++) {
+        if (!primes[i]) continue;
+
+        for (let j = i * i; j < n; j += i) {
+            primes[j] = false;
+        }
+    }
+
+    return primes.filter((x) => x).length;
+}
+
+
+
+// sieve of eratosthenes with generator: time limit exceeded
+function solution2(n: number): number {
+    let count = 0;
+    for (const x of g()) {
+        if (!(x < n)) break;
+
+        count++;
+    }
+
+    return count;
+}
+function* g(): Generator<number> {
+    const h = new Map<number, number[]>();
+    
+    let n = 2;
+    while (true) {
+        if (h.has(n)) {
+            for (const x of h.get(n)!) {
+                if (!h.has(n + x)) h.set(n + x, []);
+                h.get(n + x)!.push(x);
+            }
+            h.delete(n);
+        } else {
+            yield n;
+            h.set(n * n, [n]);
+        }
+
+        n++;
+    }
+}
